5e60b70b1782fc7acbf9ec9df5c568bfcc8dd14d
lisp <lishaopeng189@163.com>
2020-07-19 17:15:41 +0800

    Initial commit

---
+++ src/main/scala/com/lisp/flink2020/DateSetWc.scala
@@ -0,0 +1,22 @@
+package com.lisp.flink2020
+
+import org.apache.flink.api.scala.{AggregateDataSet, DataSet, ExecutionEnvironment}
+
+object DateSetWc {
+  def main(args: Array[String]): Unit = {
+    // 1 env 2source 3 transform 4 sink
+    //构造执行环境
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+    //读取文件
+    val input = "file:///d:/temp/hello.txt"
+    val ds: DataSet[String] = env.readTextFile(input)
+    // 其中flatMap 和Map 中  需要引入隐式转换
+    import org.apache.flink.api.scala.createTypeInformation
+    //经过groupby进行分组，sum进行聚合
+    val aggDs: AggregateDataSet[(String, Int)] = ds.flatMap(_.split(" ")).map((_, 1)).groupBy(0).sum(1)
+    // 打印
+    aggDs.print()
+
+
+  }
+}
